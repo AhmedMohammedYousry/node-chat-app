875e0210ccc28eab940d0c02a32107aa52790bd7
AhmedMohammedYousry <ahmedmohammedyousry33@gmail.com>
2021-02-28 13:42:11 +0200

    Render messages on the client and adjust button states

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,15 +3,37 @@
 // real time communication.
 const socket = io()
 
+// Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+
+const $sendLocationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+
 socket.on('message', (msg) => {
     console.log(msg);
+    // first Mustache compiles the template
+    const html = Mustache.render(messageTemplate, {
+        message: msg
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
 })
 
 
-document.querySelector('#message-form').addEventListener('submit', (event) => {
+$messageForm.addEventListener('submit', (event) => {
     event.preventDefault()
+    // disable form
+    $messageFormButton.setAttribute('disabled', 'disabled')
     const message = event.target.elements.message.value
     socket.emit('sendMessage', message, (error) => {
+        // enable form again here in the acknowledgment
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
         if(error) {
             return console.log(error);
         }
@@ -21,15 +43,17 @@ document.querySelector('#message-form').addEventListener('submit', (event) => {
 })
 
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser!')
     }
+    $sendLocationButton.setAttribute('disabled', 'disabled')
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, (msg) => {
+            $sendLocationButton.removeAttribute('disabled')
             console.log(msg);
         })
     })
